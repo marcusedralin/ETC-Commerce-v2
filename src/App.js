99f9848ab2cb1a49634fb8a50a8a1be61b5c8791
Marcus Edralin <medralin36@yahoo.com>
2021-07-29 11:30:32 -0700

    Front End Cart Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { commerce } from './lib/commerce';
-
-// import Products from './components/Products/Products';
-// import Navbar from './components/Navbar/Navbar';
-
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -15,15 +13,59 @@ const App = () => {
         setProducts(data);
     }
 
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+      };
+
+      const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+    
+        setCart(item.cart);
+      };
+    
+      const handleUpdateCartQty = async (lineItemId, quantity) => {
+        const response = await commerce.cart.update(lineItemId, { quantity });
+    
+        setCart(response.cart);
+      };
+    
+      const handleRemoveFromCart = async (lineItemId) => {
+        const response = await commerce.cart.remove(lineItemId);
+    
+        setCart(response.cart);
+      };
+    
+      const handleEmptyCart = async () => {
+        const response = await commerce.cart.empty();
+    
+        setCart(response.cart);
+      };
+    
+
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     }, []);
 
+    console.log(cart);
+
     return (
-        <div>
-            <Navbar />
-            <Products products={products} />
-        </div>
+        <Router>
+            <div>
+                <Navbar totalItems={cart.total_items}/>
+                <Switch>
+                    <Route exact path="/">
+                        <Products products={products} onAddToCart={handleAddToCart} />
+                    </Route>
+                    <Route exact path="/cart">
+                        <Cart 
+                            cart={cart} 
+                            onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} 
+                        />
+                    </Route> 
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -10,16 +10,16 @@ import useStyles from './styles';
 //     { id: 1, name: 'Hats', description: 'ETC Bucket Hat', price: '$19.99', image: '/assets/images/BucketETC.jpg' },
 // ];
 // src\assets
-const Products = ({ products }) => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
 
     return (
         <main className={classes.content}>
             <div className={classes.toolbar} />
-            <Grid container justify="center" spacing={4}>
+            <Grid container justifyContent="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>

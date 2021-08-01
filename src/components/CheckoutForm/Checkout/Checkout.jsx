cd7ce59c90473f62c790bcfa841c8fe4bd88bf58
Marcus Edralin <medralin36@yahoo.com>
2021-08-01 16:50:27 -0700

    Checkout/AddressForm/FormInput added

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,21 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
+import { commerce } from '../../../lib/commerce';
 import useStyles from './styles';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
 
-const steps = ['Shipping Address', 'Payment details'];
+const steps = ['Shipping address', 'Payment details'];
 
-const Checkout = () => {
+const Checkout = ({ cart }) => {
     const [activeStep, setActiveStep ] = useState(0);
+    const [checkoutToken, setCheckoutToken ] = useState(null);
     const classes = useStyles();
+
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' })
+
+                console.log(token);
+
+                setCheckoutToken(token);
+            } catch (error) {
+
+            }
+        }
+
+        generateToken();
+    }, []);
 
     const Confirmation = () => (
         <div>
             Confirmation
         </div>
-    )
+    );
 
     const Form = () => activeStep === 0 
         ? <AddressForm />
@@ -27,7 +45,8 @@ const Checkout = () => {
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
                     <Typography variant="h4" align="center">Checkout</Typography>
-                    <Stepper activeStep={0} className={classes.stepper}>
+                    <Divider className={classes.divider} />
+                    <Stepper activeStep={activeStep} className={classes.stepper}>
                         {steps.map((step) => (
                             <Step key={step}>
                                 <StepLabel>{step}</StepLabel>
